624b5d4fe2a5d9ecdfbe65206f1d7957a46cb85e
busayo <prosperotemuyiwa@gmail.com>
2015-08-24 05:43:31 +0100

    Update user model with facebook_id

--- app/User.php
+++ app/User.php
@@ -24,12 +24,12 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'facebook_id', 'avatar'];
 
     /**
      * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['remember_token'];
 }
