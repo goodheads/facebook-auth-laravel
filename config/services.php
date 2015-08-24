46e9029ca10317e0c09c857b171cc2a7f64b0aac
busayo <prosperotemuyiwa@gmail.com>
2015-08-24 05:12:06 +0100

    Add facebook config

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'secret' => '',
     ],
 
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect'      => env('CALLBACK_URL')
+    ],
+
 ];
