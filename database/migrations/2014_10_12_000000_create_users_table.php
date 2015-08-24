1dda4af0d5266ca660e562860de845170d4aaa19
busayo <prosperotemuyiwa@gmail.com>
2015-08-24 05:38:12 +0100

    Update user migration file with facebook_id column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('facebook_id')->unique();
+            $table->string('avatar');
             $table->rememberToken();
             $table->timestamps();
         });
